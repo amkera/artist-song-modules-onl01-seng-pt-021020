18bfd16429accccbede31e96178f7ea57ec47467
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-12 19:13:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,13 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
+  
   @@songs = []
+  
   def initialize
     @@songs << self
   end
+  
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
